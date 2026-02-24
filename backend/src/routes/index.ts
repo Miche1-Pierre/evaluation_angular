30ce9a07cac2fefbbe79fe28b386000a358a518c
Miche1-Pierre <pierre.michel.work@gmail.com>
2026-02-24 09:06:34 -0500

    feat(routes): add leaderboard routes to the main router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -4,6 +4,7 @@ import productsRoutes from './products.routes';
 import sessionsRoutes from './sessions.routes';
 import friendsRoutes from './friends.routes';
 import invitesRoutes from './invites.routes';
+import leaderboardRoutes from './leaderboard.routes';
 
 const router = Router();
 
@@ -13,5 +14,6 @@ router.use('/products', productsRoutes);
 router.use('/sessions', sessionsRoutes);
 router.use('/friends', friendsRoutes);
 router.use('/invites', invitesRoutes);
+router.use('/leaderboard', leaderboardRoutes);
 
 export default router;
