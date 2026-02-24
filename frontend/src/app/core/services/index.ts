9824fb2d1b51427bd238b497dd6e575425c38972
Miche1-Pierre <pierre.michel.work@gmail.com>
2026-02-24 09:36:32 -0500

    feat(services): add index file for exporting core services

---
+++ frontend/src/app/core/services/index.ts
@@ -0,0 +1,8 @@
+// Services exports
+export * from './auth.service';
+export * from './theme.service';
+export * from './products.service';
+export * from './sessions.service';
+export * from './friends.service';
+export * from './invites.service';
+export * from './game.service';
